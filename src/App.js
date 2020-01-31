ca53c43975ec7b256fe692d4d8a77ede79bc5be9
StepanNovikov <stepan.novikov.1996@gmail.com>
2020-01-31 13:26:44 +0300

    created new component such as Navbar and has connect react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import "./App.css";
 import Dashboard from "./components/dashboard/Dashboard";
+import Navbar from "./components/Navbar/Navbar";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 class App extends Component {
   render() {
     return(
-        <div className="App">
-            <Dashboard/>
-        </div>
+        <Router>
+            <div>
+                <Navbar/>
+                <Dashboard/>
+            </div>
+        </Router>
     )
   }
 }
